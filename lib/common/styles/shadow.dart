5e9ce709e5eb4d011f13287151d2ffa756a5b873
esuye <esubalewbitew13@gmail.com>
2024-02-26 12:17:30 +0300

    added common style

---
+++ lib/common/styles/shadow.dart
@@ -0,0 +1,16 @@
+import 'package:asebmarket/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class ShadowStyle {
+  static final verticalProductShadow = BoxShadow(
+      color: TColors.darkGrey.withOpacity(0.1),
+      blurRadius: 50,
+      spreadRadius: 7,
+      offset: const Offset(0, 2));
+
+  static final horizontalProductShadow = BoxShadow(
+      color: TColors.darkGrey.withOpacity(0.1),
+      blurRadius: 50,
+      spreadRadius: 7,
+      offset: const Offset(0, 2)); 
+}
