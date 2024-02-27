c73e6fbd547c8899a1e422a045dda4969ac6ac37
esuye <esubalewbitew13@gmail.com>
2024-02-27 23:37:41 +0300

    onboarding controller onprogress

---
+++ lib/features/authentication/controllers/onboarding_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+  void updatePageIndicator(index) {}
+
+  void dotNavigationonClick(index) {}
+
+  void skipPage(index) {}
+
+  void nextPage(index) {}
+}
