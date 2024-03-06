85fa8dd787474401e92e025ff3e0756d8b968588
esuye <esubalewbitew13@gmail.com>
2024-03-06 23:56:31 +0300

    bug fixes on clould helper function

--- lib/utils/helpers/cloud_helper_functions.dart
+++ lib/utils/helpers/cloud_helper_functions.dart
@@ -2,17 +2,15 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 /// Helper functions for cloud-related operations.
 class TCloudHelperFunctions {
-
   /// Helper function to check the state of a single database record.
   ///
   /// Returns a Widget based on the state of the snapshot.
   /// If data is still loading, it returns a CircularProgressIndicator.
   /// If no data is found, it returns a generic "No Data Found" message.
   /// If an error occurs, it returns a generic error message.
-  /// Otherwise, it returns null.
+
   static Widget? checkSingleRecordState<T>(AsyncSnapshot<T> snapshot) {
     if (snapshot.connectionState == ConnectionState.waiting) {
       return const Center(child: CircularProgressIndicator());
@@ -36,7 +34,11 @@ class TCloudHelperFunctions {
   /// If no data is found, it returns a generic "No Data Found" message or a custom nothingFoundWidget if provided.
   /// If an error occurs, it returns a generic error message.
   /// Otherwise, it returns null.
-  static Widget? checkMultiRecordState<T>({required AsyncSnapshot<List<T>> snapshot, Widget? loader, Widget? error, Widget? nothingFound}) {
+  static Widget? checkMultiRecordState<T>(
+      {required AsyncSnapshot<List<T>> snapshot,
+      Widget? loader,
+      Widget? error,
+      Widget? nothingFound}) {
     if (snapshot.connectionState == ConnectionState.waiting) {
       if (loader != null) return loader;
       return const Center(child: CircularProgressIndicator());
